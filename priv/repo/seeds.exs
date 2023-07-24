e7f763eeed4f3e25fd9711a267f9ee68712b9326
Patrick <patrick@pop-os.localdomain>
2023-07-24 14:37:22 -0700

    added faker and seed
    added DB seed that uses faker data.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Blog.Repo
+alias Blog.Posts.Post
+Faker.start()
+
+Enum.each(1..3, fn _ ->
+  Post.changeset(
+    %Post{},
+    %{
+      title: Faker.Lorem.Shakespeare.Ru.hamlet(),
+      sub_title: Faker.Lorem.Shakespeare.Ru.king_richard_iii(),
+      body: Faker.Lorem.Shakespeare.Ru.romeo_and_juliet()
+    }
+  )
+  |> Repo.insert!()
+end)
+
+Enum.each(1..7, fn _ ->
+  Post.changeset(
+    %Post{},
+    %{
+      title: Faker.Lorem.sentence(),
+      sub_title: Faker.Cat.name(),
+      body: Faker.Lorem.paragraphs(3) |> Enum.join("\n")
+    }
+  )
+  |> Repo.insert!()
+end)
